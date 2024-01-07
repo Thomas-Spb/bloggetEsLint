2360266c32b9d110ab5f5865f27ea6926ed54d8e
Thomas-Spb <thomas-spb@list.ru>
2024-01-07 17:40:32 +0300

    Post from map

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,18 +2,37 @@ import cls from './List.module.css';
 import Post from './Post';
 
 export const List = () => {
-  const postData = {
-    thumbnail: '',
-    title: 'Title',
-    author: 'NickName',
-    ups: 24,
-    date: '2022-02-24T09:45:00.00Z',
-  };
+  const postData = [
+    {
+      thumbnail: '',
+      title: 'Title',
+      author: 'NickName',
+      ups: 24,
+      date: '2022-02-24T09:45:00.00Z',
+      id: '567',
+    },
+    {
+      thumbnail: '',
+      title: 'Title1',
+      author: 'NickName1',
+      ups: 44,
+      date: '2022-02-24T09:45:00.00Z',
+      id: '23',
+    },
+    {
+      thumbnail: '',
+      title: 'Title2',
+      author: 'NickName2',
+      ups: 34,
+      date: '2022-02-24T09:45:00.00Z',
+      id: '36',
+    },
+  ];
   return (
     <ul className={cls.list}>
-      <Post postData={postData} />
-      <Post postData={postData} />
-      <Post postData={postData} />
+      {postData.map(postData => (
+        <Post key={postData.id} postData={postData} />
+      ))}
     </ul>
   );
 };
