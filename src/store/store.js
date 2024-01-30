3788256d471fd67d49e7f92c55ea895bf87da262
Thomas <thomas-spb@list.ru>
2024-01-30 10:45:40 +0300

    Auto stash before merge of "main" and "origin/main"

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { tokenReducer } from './tokenReducer';
 import { commentReducer } from './commentReducer';
 import { tokenMiddleware } from './tokenReducer';
-
+import { thunk } from 'redux-thunk';
 import { authReducer } from './auth/authReducer';
 import { postsReducer } from './posts/postsReducer';
-
-import { configureStore } from '@reduxjs/toolkit';
-import { postDataSlice } from './postData/postDataSlice';
+import { postDataReducer } from './postData/postDataReducer';
 
 export const store = configureStore({
   reducer: {
